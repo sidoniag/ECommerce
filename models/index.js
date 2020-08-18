5d6f78bb358e678090e2b9d97215f7a629c44ea1
idonia <sidoniag@gmail.com>
2020-08-18 17:45:23 -0500

    update index.js

--- models/index.js
+++ models/index.js
@@ -7,21 +7,22 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category,
   {
-    as: 'Product',
+    foreignKey: 'category_id',
   });
 // Categories have many Products
 Category.hasMany(Product, {
-  as: 'Category',
+    foreignKey: 'category_id',
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag,
   {
     through: 'ProductTag',
-    foreignKey: 'tagId'
+    foreignKey: 'product_id'
   });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product), {
-  as: 'Tag',
+    through: 'ProductTag',
+    foreignKey: 'tag_id',
 };
 
 module.exports = {
