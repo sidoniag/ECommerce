5e6c546f9030671779150617ac9a74dbe15f5a95
idonia <sidoniag@gmail.com>
2020-09-20 21:29:40 -0500

    update server

--- models/index.js
+++ models/index.js
@@ -4,6 +4,8 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+
+
 // Products belongsTo Category
 Product.belongsTo(Category,
   {
