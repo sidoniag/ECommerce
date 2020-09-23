3e7567f5c083b2bd19c681ee9866b3b0b2aa867b
idonia <sidoniag@gmail.com>
2020-09-23 11:00:39 -0500

    update tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -2,94 +2,66 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
-
+// find all tags
+// be sure to include its associated Product data
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
   Tag.findAll({
-    attributes: {}
+    include: [
+      {
+        model: Product,
+        through: ProductTag,
+      },
+    ],
   })
-    .then(dbEcommerce => res.json(dbEcommerce))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
+    .then((tags) => res.status(200).json(tags))
+    .catch((err) => res.status(500).json(err));
     });
-});
 
+// find a single tag by its `id`
+// be sure to include its associated Product data
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
   Tag.findOne({
-    attributes: {},
     where: {
       id: req.params.id
-    }
+    },
+    include: [
+      {
+        model: Product,
+        through: ProductTag,
+      },
+    ],
   })
-    .then(dbEcommerce => {
-      if (!dbEcommerce) {
-        res.status(404).json({ message: 'No tag found with that id'});
-        return;
-      }
-      res.json(dbEcommerce);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(404).json(err));
     });
-});
 
+// create a new tag
 router.post('/', (req, res) => {
-  // create a new tag
-  Tag.create({
-    id: req.body.id,
-    tag_name: req.body.tag_name,
-  })
-  .then(dbEcommerce => res.json(dbEcommerce))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
+  Tag.create(req.body)
+  .then((tag) => res.status(200).json(tag))
+  .catch((err) => res.status(404).json(err));
   });
-});
 
+// update a tag's name by its `id` value
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
   Tag.update(req.body, {
-    individualHooks: true,
     where: {
       id: req.params.id
-    }
+    },
   })
-    .then(dbEcommerce => {
-      if (!dbEcommerce[0]) {
-        res.status(404).json({ message: 'No tag found with that id'});
-        return;
-      }
-      res.json(dbEcommerce);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
+    .then(dbEcommerce => res.status(200).json(tag))
+    .catch((err) => res.status(404).json(err));
     });
-});
 
+// delete on tag by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
   Tag.destroy({
     where: {
       id: req.params.id
     }
   })
-    .then(dbEcommerce => {
-      if(!dbEcommerce) {
-        res.status(404).json({ message: 'No tag found with that id'});
-        return;
-      }
-      res.json(dbEcommerce);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
+    .then(dbEcommerce => res.status(200).json(tag))
+    .catch((err) =>res.status(404).json(err));
     });
-});
 
 module.exports = router;
